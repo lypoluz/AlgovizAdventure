4663563560fd62f9fc13d40b3c8202eee24949b3
Tobias <tobias.boosfeld@gmail.com>
2022-01-14 00:37:50 +0100

    Made Level

---
+++ components/Level.hpp
@@ -0,0 +1,35 @@
+//
+// Created by tobia on 13.01.2022.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef ALGOVIZADVENTURE_LEVEL_HPP
+#define ALGOVIZADVENTURE_LEVEL_HPP
+
+struct Level {
+    std::string name;
+    int xSize;
+    int ySize;
+    std::string theme;
+    vector <vector<char>> levelVector;
+
+
+    Level(std::string name, int xSize, int ySize, std::string theme, vector <vector<char>> levelVector) {
+        this->name = name;
+        this->xSize = xSize;
+        this->ySize = ySize;
+        this->levelVector = levelVector;
+    }
+
+    char getCharAt(int x, int y){
+        return levelVector
+    }
+
+};
+
+
+
+#endif //ALGOVIZADVENTURE_LEVEL_HPP
