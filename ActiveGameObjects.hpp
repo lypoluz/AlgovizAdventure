fe128830b2c1b29850b8d438ae5dbd22a487c094
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-13 02:51:27 +0100

    tracker for game objects in scene

---
+++ ActiveGameObjects.hpp
@@ -0,0 +1,26 @@
+// Created by Lypoluz (Dominik) on 13.01.2022.
+
+#ifndef ALGOVIZADVENTURE_ACTIVEGAMEOBJECTS_HPP
+#define ALGOVIZADVENTURE_ACTIVEGAMEOBJECTS_HPP
+
+#include <vector>
+#include "GameObject.hpp"
+
+class ActiveGameObjects {
+    std::vector<GameObject *> activeObjects;
+
+public:
+    std::vector<GameObject *> getActive() {
+        return activeObjects;
+    }
+
+    void add(GameObject* obj) {
+        activeObjects.push_back(obj);
+    }
+
+    void remove(GameObject* obj) {
+
+    }
+};
+
+#endif //ALGOVIZADVENTURE_ACTIVEGAMEOBJECTS_HPP
