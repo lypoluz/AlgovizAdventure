2b171e6f4bdea4d914b10ddf47a82704f6b77598
Tobias <tobias.boosfeld@gmail.com>
2022-01-13 23:44:21 +0100

    Made LevelParser

---
+++ components/LevelParser.hpp
@@ -0,0 +1,29 @@
+//
+// Created by tobia on 13.01.2022.
+//
+
+#include <iostream>
+#include <fstream>
+
+#ifndef ALGOVIZADVENTURE_LEVELPARSER_HPP
+#define ALGOVIZADVENTURE_LEVELPARSER_HPP
+
+class LevelParser {
+    string name;
+    int xSize;
+    int ySize;
+    string theme;
+    char levelArray[][];
+
+    LevelParser(string name, int xSize, int ySize, string theme, char levelArray[][]) {
+        this->name = name;
+        this-> xSize
+    }
+
+
+
+};
+
+
+
+#endif //ALGOVIZADVENTURE_LEVELPARSER_HPP
