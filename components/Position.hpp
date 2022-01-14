19a52b02b10e35640fcd858b18e1e7134fe260fd
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-14 22:44:07 +0100

    rotation

--- components/Position.hpp
+++ components/Position.hpp
@@ -8,10 +8,14 @@
 
 class Position : public GameComponent {
     Vector2 position;
+    float rotation;
     bool dirty{};
 
 public:
-    explicit Position(ObjectStructure* os) : GameComponent(os) {}
+    explicit Position(ObjectStructure* os) : GameComponent(os) {
+        position = Vector2::zero();
+        rotation = 0;
+    }
 
     void preUpdate() override {dirty = false;}
     std::string getName() override {return "Position";}
@@ -19,12 +23,15 @@ public:
     Vector2 getPosition() {return position;}
     float x() {return position.x();}
     float y() {return position.y();}
+    int getRotation() {return rotation;}
 
     void moveTo(Vector2 newPos) {position = newPos; dirty = true;}
     void moveTo(float x, float y) { moveTo(Vector2(x,y));}
-
     void moveBy(Vector2 amount) {position += amount; dirty = true;}
     void moveBy(float x, float y) {moveBy(Vector2(x,y));}
+
+    void rotateTo(float angle) {rotation = angle; dirty = true;}
+    void rotateBy(float angle) {rotation += angle;}
 
     bool isDirty() {return dirty;}
 };
