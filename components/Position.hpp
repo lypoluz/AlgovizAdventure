92983ebc41d84031b687a4de3ed94e3e2f4611bf
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-17 23:03:46 +0100

    lastMoveVector

--- components/Position.hpp
+++ components/Position.hpp
@@ -7,12 +7,14 @@
 #include "../Vector2.hpp"
 
 class Position : public GameComponent {
+    Vector2 lastPosition;
     Vector2 position;
     float rotation;
     bool dirty{};
 
 public:
     explicit Position(ObjectStructure* os) : GameComponent(os) {
+        lastPosition = Vector2::zero();
         position = Vector2::zero();
         rotation = 0;
     }
@@ -20,14 +22,16 @@ public:
     void preUpdate() override {dirty = false;}
     std::string getName() override {return "Position";}
 
+    Vector2 getLastMoveVector() {return position - lastPosition;}
     Vector2 getPosition() {return position;}
     float x() {return position.x();}
     float y() {return position.y();}
+
     int getRotation() {return rotation;}
 
-    void moveTo(Vector2 newPos) {position = newPos; dirty = true;}
+    void moveTo(Vector2 newPos) {lastPosition = position; position = newPos; dirty = true;}
     void moveTo(float x, float y) { moveTo(Vector2(x,y));}
-    void moveBy(Vector2 amount) {position += amount; dirty = true;}
+    void moveBy(Vector2 amount) {lastPosition = position; position += amount; dirty = true;}
     void moveBy(float x, float y) {moveBy(Vector2(x,y));}
 
     void rotateTo(float angle) {rotation = angle; dirty = true;}
