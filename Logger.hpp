14662c76b42eb168b880c4537a78a7c253140c97
Tobias <tobias.boosfeld@gmail.com>
2022-01-19 16:14:23 +0100

    made logger

---
+++ Logger.hpp
@@ -0,0 +1,21 @@
+//
+// Created by tobia on 19.01.2022.
+//
+
+#ifndef ALGOVIZADVENTURE_LOGGER_HPP
+#define ALGOVIZADVENTURE_LOGGER_HPP
+
+#include <fstream>
+#include <string>
+
+struct Logger{
+
+    static void logThing(std::string input){
+        std::ofstream file("logfile.txt");
+        file << input << "\n";
+        file.close();
+    }
+
+};
+
+#endif //ALGOVIZADVENTURE_LOGGER_HPP
