dff3a77eea0a7754365c961bd126fc54f6a45474
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-19 13:40:09 +0100

    config

---
+++ Config.hpp
@@ -0,0 +1,14 @@
+// Created by Lypoluz (Dominik) on 19.01.2022.
+
+#ifndef ALGOVIZADVENTURE_CONFIG_HPP
+#define ALGOVIZADVENTURE_CONFIG_HPP
+
+struct Config {
+    bool expandAlgoViz = true;
+    bool fullscreen = true;
+    int windowSize = 480;
+    int windowFrameSizeFactor = 24;
+    float playerSpeed = 2.5;
+    bool funnyEnemies = false;
+};
+#endif //ALGOVIZADVENTURE_CONFIG_HPP
