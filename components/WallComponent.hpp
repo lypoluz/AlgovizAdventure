3c854ffa0818be5f91eb162b2c526ac65d5bdd86
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-19 13:27:37 +0100

    WallComponent: initial commit

---
+++ components/WallComponent.hpp
@@ -0,0 +1,16 @@
+// Created by Lypoluz (Dominik) on 19.01.2022.
+
+#ifndef ALGOVIZADVENTURE_WALLCOMPONENT_HPP
+#define ALGOVIZADVENTURE_WALLCOMPONENT_HPP
+
+#include "engine/GameComponent.hpp"
+
+class WallComponent : public GameComponent {
+
+public:
+    WallComponent(ObjectStructure* os) : GameComponent(os) {}
+
+
+};
+
+#endif //ALGOVIZADVENTURE_WALLCOMPONENT_HPP
