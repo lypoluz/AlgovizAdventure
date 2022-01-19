c0c842885e8672f1f897c37e2dd69322bc418e9c
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-19 16:59:14 +0100

    log

--- components/SpriteRenderer.hpp
+++ components/SpriteRenderer.hpp
@@ -7,15 +7,22 @@
 #include "../AlgoWrapper.hpp"
 #include "../engine/GameObject.hpp"
 #include "abstract/Renderer.hpp"
+#include "Logger.hpp"
 
 class SpriteRenderer : public Renderer {
     AlgoWrapper::Sprite sprite{};
     Vector2 dimension;
 
 public:
-    SpriteRenderer(ObjectStructure* os, AlgoWrapper::Window* win) : Renderer(os, win) {dimension = Vector2::zero(); }
+    SpriteRenderer(ObjectStructure* os, AlgoWrapper::Window* win) : Renderer(os, win) {dimension = Vector2::one(); }
 
     void setSprite(const std::string& pathToSprite) {
+        Logger::log("setting spite");
+        Logger::log(pathToSprite);
+        Logger::log(position->getPosition().toString());
+        Logger::log(dimension.toString());
+        Logger::log(reinterpret_cast<const char *>(window));
+
         sprite = AlgoWrapper::Sprite(pathToSprite, position->getPosition(), dimension, window);
         sprite.setAttribute("image-rendering", "crisp-edges");
         element = &sprite;
