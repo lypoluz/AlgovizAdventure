c7673af22ec4150ebac02d3960ebfe66c55f3be3
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-19 14:36:21 +0100

    create wall example

---
+++ documentation/CreateWall.hpp
@@ -0,0 +1,35 @@
+// Created by Lypoluz (Dominik) on 19.01.2022.
+
+#ifndef ALGOVIZADVENTURE_CREATE_HPP
+#define ALGOVIZADVENTURE_CREATE_HPP
+
+#include "engine/ActiveGameObjects.hpp"
+#include "engine/Engine.hpp"
+#include "WallComponent.hpp"
+#include "SpriteRenderer.hpp"
+
+void myFunction() {
+
+    for (int i = 0; i < 10; ++i) {
+        ActiveGameObjects* ago = Engine::getInstance()->getAGO();
+        AlgoWrapper::Window* window = Engine::getInstance()->getGameWindow();
+
+        GameObject* wall = new GameObject("wall" + i);
+        ago->add(wall);
+
+        Position* position = new Position(wall);
+        wall->addPosition(position);
+
+        WallComponent* wallComp = new WallComponent(wall);
+        wall->addComponent(wallComp);
+
+        auto* renderer = new SpriteRenderer(wall, window);
+        renderer->setSprite("PathToPNG");
+        renderer->setSize({16,16});
+        wall->addComponent(renderer);
+
+    }
+
+}
+
+#endif //ALGOVIZADVENTURE_CREATE_HPP
