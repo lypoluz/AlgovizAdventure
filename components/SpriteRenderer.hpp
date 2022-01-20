ec02216fede6e9e05fea5e324ef6cc20c2d11189
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-20 14:51:32 +0100

    getSpritePath

--- components/SpriteRenderer.hpp
+++ components/SpriteRenderer.hpp
@@ -12,11 +12,13 @@
 class SpriteRenderer : public Renderer {
     AlgoWrapper::Sprite sprite{};
     Vector2 dimension;
+    std::string currentSprite;
 
 public:
     SpriteRenderer(ObjectStructure* os, AlgoWrapper::Window* win) : Renderer(os, win) {dimension = Vector2::one(); }
 
     void setSprite(const std::string& pathToSprite) {
+        currentSprite = pathToSprite;
         sprite = AlgoWrapper::Sprite(pathToSprite, position->getPosition() * 16, dimension, window);
         sprite.setAttribute("image-rendering", "crisp-edges");
         sprite.setAttribute("shape-rendering", "crispEdges");
@@ -24,6 +26,8 @@ public:
     }
 
     void setSize(Vector2 d) {dimension = d; sprite.setSize(dimension);}
+
+    std::string getSpritePath() {return currentSprite;}
 };
 
 #endif //ALGOVIZADVENTURE_SPRITERENDERER_HPP
