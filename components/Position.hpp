2b9f83afd898f3f728e43d020cec9d2f021199b6
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-20 15:39:42 +0100

    fixed facing

--- components/Position.hpp
+++ components/Position.hpp
@@ -9,31 +9,34 @@
 class Position : public GameComponent {
     Vector2 lastPosition;
     Vector2 position;
-    float rotation;
+    Vector2 lastFacing;
+    float rotation{};
     bool dirty = true;
 
-public:
-    explicit Position(ObjectStructure *os) : GameComponent(os) {
-        position = Vector2::zero();
-        rotation = 0;
+    void updateFacing() {
+        Vector2 facing = (position-lastPosition).normalized();
+        lastFacing = (facing == Vector2::zero()) ? lastFacing : facing;
     }
+
+public:
+    explicit Position(ObjectStructure *os) : GameComponent(os) {}
 
     void preUpdate() override { dirty = false; }
 
     std::string getName() override { return "Position"; }
 
-    Vector2 facing() { return (position - lastPosition).normalized();}
+    Vector2 facing() { return lastFacing;}
     Vector2 getPosition() { return position; }
     float x() { return position.x(); }
     float y() { return position.y(); }
-    float getRotation() { return rotation; }
-
+    float getRotation() const { return rotation; }
 
     void moveTo(float x, float y) { moveTo(Vector2(x, y)); }
     void moveTo(Vector2 newPos) {
         lastPosition = position;
         position = newPos;
         dirty = true;
+        updateFacing();
     }
 
     void moveBy(float x, float y) { moveBy(Vector2(x, y)); }
@@ -41,6 +44,7 @@ public:
         lastPosition = position;
         position += amount;
         dirty = true;
+        updateFacing();
     }
 
     void rotateTo(float angle) {
@@ -50,7 +54,7 @@ public:
 
     void rotateBy(float angle) { rotation += angle; }
 
-    bool isDirty() { return dirty; }
+    bool isDirty() const { return dirty; }
 };
 
 #endif //ALGOVIZADVENTURE_POSITION_HPP
