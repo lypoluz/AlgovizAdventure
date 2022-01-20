7f9b7785b9c469810d4276c68ceb790036cd91ea
Tobias <tobias.boosfeld@gmail.com>
2022-01-20 16:04:27 +0100

    "made" Special symbol resolver (rudimentary)

---
+++ levelSystem/SpecialLevelSymbols.hpp
@@ -0,0 +1,21 @@
+//
+// Created by tobia on 20.01.2022.
+//
+
+#ifndef ALGOVIZADVENTURE_SPECIALLEVELSYMBOLS_HPP
+#define ALGOVIZADVENTURE_SPECIALLEVELSYMBOLS_HPP
+
+#include <string>
+
+class SpecialLevelSymbols{
+
+
+public:
+
+    static void interpretSymbol(std::string symbol) {
+        //place symbol here
+    }
+
+};
+
+#endif //ALGOVIZADVENTURE_SPECIALLEVELSYMBOLS_HPP
