383e49c1e2145d8e9317a798634ec50045014670
Tobias <tobias.boosfeld@gmail.com>
2022-01-21 10:17:58 +0100

    added functionality

--- levelSystem/SpecialLevelSymbols.hpp
+++ levelSystem/SpecialLevelSymbols.hpp
@@ -6,14 +6,26 @@
 #define ALGOVIZADVENTURE_SPECIALLEVELSYMBOLS_HPP
 
 #include <string>
+#include "LevelBuilder.hpp"
 
 class SpecialLevelSymbols{
 
 
 public:
 
-    static void interpretSymbol(int x, int y, std::string symbol) {
+    static void interpretSymbol(int x, int y, std::string& theme, ActiveGameObjects* ago, AlgoWrapper::Window* window, std::string symbol) {
         //place symbol here
+        if (symbol.substr(0, (symbol.find(' ')-1) ) == "enemy") {
+            // enemy code
+        }else if (symbol.substr(0, (symbol.find(' ')-1) ) == "roomlink") {
+            // roomlink code
+        }else if (symbol.substr(0, (symbol.find(' ')-1) ) == "spawnPoint" ){
+            // place floor
+            LevelBuilder::placeFloor(x, y, theme, ago, window);
+        }else if (symbol.substr(0, (symbol.find(' ')-1) ) == "item"){
+            // item code
+        }
+
     }
 
 };
