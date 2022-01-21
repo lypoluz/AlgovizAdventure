67e7d91d98843d7ff8b6b5523b5027e71e7da401
Tobias <tobias.boosfeld@gmail.com>
2022-01-21 09:43:47 +0100

    added corridor functionality

--- levelSystem/LevelBuilder.hpp
+++ levelSystem/LevelBuilder.hpp
@@ -130,6 +130,29 @@ class LevelBuilder{
         floor->addComponent(renderer);
     }
 
+    static void placeCorridor(int x, int y, const std::string& theme, ActiveGameObjects* ago, AlgoWrapper::Window* window){
+        auto* block = new GameObject("block" + std::to_string(x) + std::to_string(y));
+        ago->add(block);
+
+        //instantiate position component
+        auto* position = new Position(block);
+
+        //set position to value
+        position->moveTo(x, y);
+
+        //game object gets its position
+        block->addPosition(position);
+
+        auto* blockComp = new WallComponent(block);
+        block->addComponent(blockComp);
+
+
+        auto* renderer = new SpriteRenderer(block, window);
+        renderer->setSize({16,16});
+        renderer->setSprite("sprites/" + theme + "/corridor.svg");
+        block->addComponent(renderer);
+    }
+
 
 
 public:
@@ -171,6 +194,7 @@ public:
 
                     case 'B':
                         //place black floor
+                        placeCorridor(x, y, level.theme, ago, window);
                         break;
 
                     case 'C':
