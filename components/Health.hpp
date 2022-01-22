c0d7e65b6d88851e421e4d5d46f154b71f85654c
annika <annikamoritz@gmail.com>
2022-01-22 17:34:16 +0100

    besiegete Entity in health

--- components/Health.hpp
+++ components/Health.hpp
@@ -11,15 +11,33 @@
 class Health : public GameComponent {
 
     int health;
+    ObjectStructure* objs;
 
 public:
-    explicit Health(ObjectStructure* os) : GameComponent(os) {}
+    explicit Health(ObjectStructure* os) : GameComponent(os) {
+        objs = os;
+    }
 
     void setHealth(int newHealth) {health = newHealth;}
 
     void update() override {
         if (health == 0) {
-//            ActiveGameObjects::remove(gameObject);
+            if(objs ==){
+//                game over
+            }
+            else{
+//                entferne enemy
+//                ActiveGameObjects::remove(gameObject);
+            }
+            GameObject* player;
+            for (GameObject *go: activeObjects) {
+                if (go->getName() == "Player") {
+                    player = go;
+                } else {
+                    go->onDestroy();
+                    delete go;
+                }
+            }
         }
     }
 };
