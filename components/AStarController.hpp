7efbd9641a10b4ed5f409e8151fd03c2a7a9decb
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-22 12:07:39 +0100

    log-- log++

--- components/AStarController.hpp
+++ components/AStarController.hpp
@@ -42,11 +42,12 @@ public:
             return Vector2::zero();
         }
         std::vector<Vector2> path = pf.tracePath();
-        if(pathRenderer != nullptr)
-            pathRenderer->setPath(SVGPathFromVector2Vector(path, 16));
+        if (pathRenderer != nullptr) {
+            std::string d = SVGPathFromVector2Vector(path, 16);
+            Logger::logln(d);
+            pathRenderer->setPath(d); }
         path.erase(path.begin());
         Vector2 direction = (path[0] - position->getPosition()).normalized();
-        Logger::logln("[A*C] next Node: " + path[0].toString() + " dir: " + direction.toString());
         return direction;
     }
 
