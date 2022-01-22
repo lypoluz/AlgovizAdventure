b58d0515169564f10556cc929e72cbc5ac30c560
Tobias <tobias.boosfeld@gmail.com>
2022-01-22 19:55:25 +0100

    added comment to add some clarity since my obvious function call apparently wasn't clear enough or so I'm told

--- levelSystem/LevelBuilder.hpp
+++ levelSystem/LevelBuilder.hpp
@@ -281,7 +281,9 @@ public:
 
                         if (symbol == "roomlink") {
                             //roomLink code
-                            createRoomLink(x, y, level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].find(' ')),level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].length()-1).substr(level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].length()-1).find(' ')+1, level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].length()-1).length()-1), ago);
+                            // roomlink [targetLevel] [linkPoint] -> targetLevel
+                            // roomlink [targetLevel] [linkPoint] -> [targetLevel] [linkPoint]-> [linkPoint]
+                            createRoomLink(x, y,level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].find(' ')),level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].length()-1).substr(level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].length()-1).find(' ')+1, level.specialSymbols[levelArray[x][y]].substr(level.specialSymbols[levelArray[x][y]].find(' ')+1, level.specialSymbols[levelArray[x][y]].length()-1).length()-1), ago);
                             placeFloor(x,y, level.theme, ago, window);
                         }else if (symbol == "linkPoint") {
                             //linkPoint code
