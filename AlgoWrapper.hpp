cafafbe8b0b37491506870035bb5293503350531
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-22 12:35:26 +0100

    bug--

--- AlgoWrapper.hpp
+++ AlgoWrapper.hpp
@@ -153,7 +153,7 @@ public:
         void setStrokeWidth(int width) override {path.setStrokeWidth(width);}
         void setAttribute(std::string attr, std::string val) override {path.setAttribute(attr, val);}
 
-        void setPath(const std::string& p) {path.setPath(p);}
+        void setPath(const std::string& p) {path.setPath(p);setAttribute("transform", "");}
     };
 
 };
