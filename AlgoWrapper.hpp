10640ad926ecab426a0741ad8f9e1f8a228d3254
lypoluz <37896314+lypoluz@users.noreply.github.com>
2022-01-22 05:29:26 +0100

    bug fix

--- AlgoWrapper.hpp
+++ AlgoWrapper.hpp
@@ -135,7 +135,7 @@ public:
         Path path;
     public:
         svgPath(std::string p, Window* window) {path = Path(p, window->getSvg());}
-        svgPath(const svgPath& p) {path = svgPath(path);}
+        svgPath(const svgPath& p) {path = Path(path);}
 
         int getX() override {return path.getX();}
         int getY() override {return path.getY();}
