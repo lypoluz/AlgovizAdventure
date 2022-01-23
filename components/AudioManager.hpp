41e08b43aa4afa5a86d8c8e2968e114499d1dbae
Tobias <tobias.boosfeld@gmail.com>
2022-01-23 15:33:27 +0100

    audiolevel is now a thing

--- components/AudioManager.hpp
+++ components/AudioManager.hpp
@@ -10,18 +10,20 @@
 class AudioManager : public GameComponent {
     bool alreadyPlayedStart = false;
     bool alreadyPlayed01= false;
+    float audioLevel = 1;
 
 public:
     AudioManager(ObjectStructure* os) : GameComponent(os) {}
 
     void onStart() override {
+        audioLevel = Engine::getInstance()->getConfig()->audioLevel;
         std::string levelName = Engine::getInstance()->getCurrentLevel().name;
         Logger::logln("[AM] playing sound of " + levelName);
         if(levelName == "start.level" && !alreadyPlayedStart) {
-            AudioPlayer::play("start.mp3");
+            AudioPlayer::play("start.mp3", audioLevel);
             alreadyPlayedStart = true;
         }else if (levelName == "01.level" && !alreadyPlayed01){
-            AudioPlayer::play("01.mp3");
+            AudioPlayer::play("01.mp3", audioLevel);
             alreadyPlayed01 = true;
         }
     }
